0a3d83a6240ca51de139abb3a95a89b6b4a73347
hassanmubiru <hassanmubiru7@gmail.com>
2026-07-07 23:43:03 +0300

    deleted:    .release-verify-tmp/README.md 	deleted:    .release-verify-tmp/package-lock.json 	deleted:    .release-verify-tmp/package.json 	deleted:    .release-verify-tmp/src/main.ts 	deleted:    .release-verify-tmp/tsconfig.json 	deleted:    .release-verify-tmp2/package-lock.json 	deleted:    .release-verify-tmp2/package.json 	deleted:    .release-verify-tmp2/src/main.ts 	deleted:    .release-verify-tmp2/tsconfig.json

--- .release-verify-tmp2/src/main.ts
+++
@@ -1,5 +0,0 @@
-import 'reflect-metadata';
-import { streetApp } from 'streetjs';
-import { StreetWebSocketServer } from 'streetjs/websocket';
-const app = streetApp({ port: 3000 });
-const wss = new StreetWebSocketServer({} as any);
